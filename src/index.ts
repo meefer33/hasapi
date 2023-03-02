0ba31b75cb411831384d3f80e938081fd1762fca
Jeff Keith <me@jeffkeith.me>
2023-03-02 05:37:56 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.get('/auth', (request, response) => {
   console.log('hit')
   const token = request.get('Authorization');
-  console.log('token',token)
+  console.log('token',token,request)
   if(token){
     response.json({
       'X-Hasura-User-Id': '25',
