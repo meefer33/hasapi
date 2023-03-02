fb7e5083e8591852d991b8d7b97fcf80a6495d5a
Jeff Keith <me@jeffkeith.me>
2023-03-02 08:12:18 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
@@ -7,16 +7,34 @@ import auth from './auth/auth'
 const port = process.env.PORT || 3000
 app.use(express.json())
 
+app.post('/:route', (req, res) => {
+  try {
+    const handler = require(`./handlers/${req.params.route}`)
+    if (!handler) {
+      return res.status(404).json({
+        message: `not found`,
+      })
+    }
+    return handler(req, res)
+  } catch (e) {
+    console.error(e)
+    return res.status(500).json({
+      message: `unexpected error occured`,
+    })
+  }
+})
+
+/*
 app.get('/auth', (request, response) => {
   console.log('hit')
-  const token = request.get('Authorization');
-  console.log('token',token)
-  if(token){
+  const token = request.get('Authorization')
+  console.log('token', token)
+  if (token) {
     response.json({
       'X-Hasura-User-Id': '25',
       'X-Hasura-Role': 'user',
     })
-  }else{
+  } else {
     response.json({
       'X-Hasura-Role': 'public',
     })
@@ -24,6 +42,7 @@ app.get('/auth', (request, response) => {
 })
 
 app.use(auth)
+*/
 
 app.listen(port, () => {
   console.log(`Auth server running on port ${port}.`)
