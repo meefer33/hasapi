4d716b7902688a0bc6eaa59f62710cac85f36367
Jeff Keith <me@jeffkeith.me>
2023-03-05 07:31:06 -0500

    init

--- src/index.ts
+++ src/index.ts
@@ -2,47 +2,41 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
-import auth from './auth/auth'
 
 const port = process.env.PORT || 3000
 app.use(express.json())
-
-app.post('/:route', (req, res) => {
+app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/:type/:route', (req, res) => {
   try {
-    const handler = require(`./handlers/${req.params.route}`)
+    const handler = require(`./handlers/${req.params.type}/${req.params.route}`)
     if (!handler) {
-      return res.status(404).json({
-        message: `not found`,
+      res.status(404).json({
+        message: 'not found',
+        extensions: {
+          code: 404,
+        },
       })
     }
-    return handler(req, res)
+    handler(req, res)
   } catch (e) {
     console.error(e)
-    return res.status(500).json({
-      message: `unexpected error occured`,
+    res.status(400).json({
+      message: 'unexpected error occured',
+      extensions: {
+        code: 400,
+      },
     })
   }
 })
 
-/*
-app.get('/auth', (request, response) => {
-  console.log('hit')
-  const token = request.get('Authorization')
-  console.log('token', token)
-  if (token) {
-    response.json({
-      'X-Hasura-User-Id': '25',
-      'X-Hasura-Role': 'user',
-    })
-  } else {
-    response.json({
-      'X-Hasura-Role': 'public',
-    })
-  }
+app.all('*', function (req, res) {
+  res.status(404).send({
+    message: 'not found',
+    extensions: {
+      code: 404,
+    },
+  })
 })
-
-app.use(auth)
-*/
 
 app.listen(port, () => {
   console.log(`Auth server running on port ${port}.`)
